982587bb89dbbfaf41b7c92e36d68d8a3ac25348
prawin <21ucs033@loyolacollege.edu>
2023-08-22 11:11:05 +0530

    added year

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask,jsonify, render_template, url_for, redirect, request, flash, send_from_directory
-import httpx
+from flask import Flask, render_template, url_for, redirect, request, flash, send_from_directory
+import requests
+from requests import adapters
+import ssl
+from urllib3 import poolmanager
 import json
 import re
+
 
 app = Flask(__name__)
 app.secret_key = 'nathaanpuluthi'
@@ -11,7 +15,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/result', methods=['POST', 'GET'])
-async def result():
+def result():
     if request.method == 'POST':
         user = request.form['username']
         pas = request.form['password']
@@ -19,59 +23,83 @@ async def result():
         result_url = 'https://mgc.ibossems.com/student/'
         list_url = 'https://mgc.ibossems.com/student/attendance/list'
         details_url = 'https://mgc.ibossems.com/student/studentdetails'
-        httpx._config.DEFAULT_CIPHERS += ":ALL:@SECLEVEL=1"
-        async with httpx.AsyncClient(follow_redirects=True) as client:
-            response1 = await client.get(form_url)
+        class TLSAdapter(adapters.HTTPAdapter):
 
-            payload = {
+            def init_poolmanager(self, connections, maxsize, block=False):
+                ctx = ssl.create_default_context()
+                ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+                self.poolmanager = poolmanager.PoolManager(
+                        num_pools=connections,
+                        maxsize=maxsize,
+                        block=block,
+                        ssl_version=ssl.PROTOCOL_TLS,
+                        ssl_context=ctx)
+
+        session = requests.session()
+        session.mount('https://', TLSAdapter())
+        response1 = session.get(form_url)
+        payload = {
                 'username': user,
-                'password': pas
+                'password':pas
             }
-
-            lis = {
-                'task': 'LISTING'
-            }
-
-            lis_headers = {
-                'X-Requested-With': 'XMLHttpRequest',
-                'Sec-Fetch-Mode': 'cors',
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
-
-            response2 = await client.post(result_url, data=payload)
-
-            error = re.findall(r'<p[^>]*>(.*?)</p>', response2.text)
-
-            if error:
+        lis = {
+            'task':'LISTING'
+        }
+        lis_headers = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Sec-Fetch-Mode': 'cors',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        response2 = session.post(result_url, data=payload)
+        error = re.findall(r'<p[^>]*>(.*?)</p>', response2.text)
+        
+        if error:
                 flash(error[0])
                 return redirect(url_for('home'))
+        
+        if response2.status_code == 200:
+            response3 = session.get(details_url, headers=lis_headers)
+            if response3.status_code == 200:
+                match = re.search(r"name:'student_name',\s+value:'([^']+)'", response3.text)
+                department_match = re.search(r"name:'department',\s+value:'([^']+)'", response3.text)
+                academic_match = re.search(r"name:'academic',\s+value:'([^']+)'", response3.text)
+                student_name = match.group(1).strip() if match else None
+                sem = academic_match.group(1) if academic_match else None
+                dept = department_match.group(1) if department_match else None
+                    
 
-            if response2.status_code == 200:
-                response3 = await client.get(details_url, headers=lis_headers)
-
-                if response3.status_code == 200:
-                    match = re.search(r"name:'student_name',\s+value:'([^']+)'", response3.text)
-                    department_match = re.search(r"name:'department',\s+value:'([^']+)'", response3.text)
-                    academic_match = re.search(r"name:'academic',\s+value:'([^']+)'", response3.text)
-                    student_name = match.group(1).strip() if match else None
-                    sem = academic_match.group(1) if academic_match else None
-                    dept = department_match.group(1) if department_match else None
-
-                lis_response = await client.post(list_url, headers=lis_headers, data=lis)
-
-                if lis_response.status_code == 200:
-                    data = json.loads(lis_response.text)
-                    present = [i['present'] for i in data['attends']]
-                    tot = [i['total'] for i in data['attends']]
-                    att_percent = round(sum(present) / sum(tot) * 100, 2)
-                    total_day = len(data['attends'])
-                    attendance_data = {
-                        'Present': att_percent,
-                        'Absent': 100 - att_percent
-                    }
-
-                    return render_template('result.html', name=student_name, att=att_percent, 
-                                           semester=sem,dept=dept, attendance_data=attendance_data, total_days=total_day)
+            lis_response = session.post(list_url, headers=lis_headers, data=lis)
+            if lis_response.status_code == 200:
+                data = json.loads(lis_response.text)
+                send = {'od': [], 'absent': [], 'full_abs': []}
+                for attend in data['attends']:
+                    if 'OD' in attend.values():
+                        desired_keys = [key for key, value in attend.items() if value == 'OD']
+                        send['od'].append({attend['date']: desired_keys})
+                    
+                    if 0 < attend['absent'] < 5:
+                        tmp = [key for key, value in attend.items() if value == 'A']
+                        send['absent'].append({attend['date']: tmp})
+                    
+                    if attend['absent'] == 5:
+                        send['full_abs'].append(attend['date'])
+        
+                present = [i['present'] for i in data['attends']]
+                tot = [i['total'] for i in data['attends']]
+                att_percent = round(sum(present) / sum(tot) * 100, 2)
+                total_day = len(data['attends'])
+                attendance_data = {
+                    'Present': att_percent,
+                    'Absent': 100 - att_percent
+                }
+            if int(sem[-1])== 1 or int(sem[-1])== 2:
+                year = '1st Year'
+            elif int(sem[-1])== 3 or int(sem[-1])== 4:
+                year = '2nd Year'
+            else:
+                year = '3rd Year'
+        return render_template('result.html', name=student_name, att=att_percent, att_details = send,
+                    semester=sem,year=year,dept=dept, attendance_data=attendance_data, total_days=total_day)
     elif request.method == 'GET':
         return redirect(url_for('home'))
 
